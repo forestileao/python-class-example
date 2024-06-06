3cf70ec7682f7a467249a65563c36b707c578eb1
korioz722205 <simonchrenko@gmail.com>
2024-06-06 20:20:57 +0100

    created main tic-tac-toe functions

--- main.py
+++ main.py
@@ -26,4 +26,23 @@ def print_game_state(game_matrix: list[list]) -> None:
       print(element, end=' ')
     print(']')
 
-print_game_state(game_state)
+
+def check_box(game_matrix, line_number, col_number, marker):
+  row = game_matrix[line_number]
+  row[col_number] = marker
+
+
+
+def is_final_state(game_matrix):
+  for row in game_matrix:
+    if row[0] == row[1] and row[1] == row[2]:
+      return True 
+  for collumn in range (0,2):
+    if game_matrix[0][collumn] == game_matrix[1][collumn] and game_matrix[1][collumn] == game_matrix[2][collumn]:
+      return True
+  if game_matrix[0][0] == game_matrix[1][1] and game_matrix[1][1] == game_matrix[2][2]:
+    return True
+  if game_matrix[0][2] == game_matrix[1][1] and game_matrix[1][1] == game_matrix[2][0]:
+    return True
+  
+  
