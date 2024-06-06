13dfb74edc9740b2383d3ac386a5d10aba01128c
Pedro Leão <pedroforestileao@gmail.com>
2024-06-06 15:27:39 -0300

    added print game state

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-def print_array(arr):
-    for i in arr:
-        print(i)
+player_1_simbol = 1
+player_2_simbol = 2
 
 
-array = [1,2, 3,4]
-print_array(array)
+game_state = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+]
+
+'''
+  output:
+    [0 0 0]
+    [0 0 0]
+    [0 0 0]
+
+    OR
+
+    [1 0 0]
+    [0 1 0]
+    [0 2 1]
+'''
+def print_game_state(game_matrix: list[list]) -> None:
+  for row in game_matrix:
+    print('[', end='')
+    for element in row:
+      print(element, end=' ')
+    print(']')
+
+print_game_state(game_state)
